f2279f7ca0fa23976113c6c592d584587bbd8a76
Captain Storm <CaptainStorm21@users.noreply.github.com>
2021-10-13 21:26:46 -0700

    configutration of the api

---
+++ server.js
@@ -0,0 +1,9 @@
+const path = require ('path');
+const express = require('express');
+const app = express();
+
+const DIR = './uploads';
+
+app.use(express.static(DIR));
+const PORT = 3500;
+app.listen(PORT, ()=> console.log('Service started on port ' + PORT) );
